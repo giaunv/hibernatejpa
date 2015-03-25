e2528b1c98dff6ee6067e5d5244c7c0c0408ef15
Giau Ngo <giaunv@gmail.com>
2015-03-25 08:36:43 +0700

    Spring ORM: Inject PersisstentContext

--- src/main/java/tutorial/UsersController.java
+++ src/main/java/tutorial/UsersController.java
@@ -1,17 +1,23 @@
 package tutorial;
 
-/**
- * Created by Giau on 3/23/2015.
- */
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ * Created by Giau on 3/25/2015.
+ */
 public class UsersController {
+    @PersistenceContext
+    private EntityManager entityManager;
 
     @RequestMapping("/users")
     public String users(Model model) {
+
+        model.addAttribute("users", entityManager.createQuery("select u from User u").getResultList());
+
         return "users";
     }
 }
